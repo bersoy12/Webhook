7f2dc258d345f9b83748cd382d713c5449fc0f03
bersoy12 <bersoy12@gmail.com>
2021-11-29 16:01:51 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,31 +1,33 @@
 import json
 import os
 import requests
+import ast 
 
 from flask import Flask
 from flask import request
 from flask import make_response
 
 app = Flask(__name__)
+api_key = 'ed43736919fd647843e9723b61be0e50'
 
-@app.route("/webhook", methods=["POST"])
+@app.route('/webhook', methods=['POST'])
 
 def webhook():
-    req = request.get_json(silent=True, force=True)
-    print(json.dumps(req, indent=4))
-
+    req = request.get_data().decode('utf-8')
+    req = json.loads(req)
     res = makeResponse(req)
     res = json.dumps(res, indent=4)
     r = make_response(res)
-    r.headers["Content-Type"] = "application/json"
+    r.headers['Content-Type'] = 'application/json'
     return r
 
 def makeResponse(req):
     result = req.get("queryResult")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    date = parameters.get("date")
-    r = requests.get("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=ed43736919fd647843e9723b61be0e50")
+    city = parameters.get("city")
+    date = parameters.get("date")[:10]
+    url_ = str('http://api.openweathermap.org/data/2.5/forecast?q=' + str(city) + '&appid=' + str(api_key))
+    r = requests.get(url_)
     json_object = r.json()
     weather = json_object["list"]
     
@@ -34,7 +36,7 @@ def makeResponse(req):
             condition = weather[i]["weather"][0]["description"]
             break
 
-    speech = city + " için " + date + " tarihinde hava durumu " + condition
+    speech = city + " şehrinde " + date + " tarihinde hava durumu " + condition
     return {
         "fulfillmentText": speech
         # "speech": speech,
